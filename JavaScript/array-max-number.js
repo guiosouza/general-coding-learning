7132ab8b314ba2fb1eec144d835fad38981ca6bd
guiosouza <guilhermedeoliveiradesouza@gmail.com>
2023-01-21 19:47:53 -0300

    Max Number Array EX Complete

--- JavaScript/array-max-number.js
+++ JavaScript/array-max-number.js
@@ -1,1 +1,17 @@
-// Ache o maior número do array:
+// Ache o maior número do array usando uma função que receça o array como argumento. Exemplo:
+// function maxNumber(myArray)
+
+let numbers = [1, 7, 39, 12, 5, 34]
+
+function maxNumber(myArray) {
+    let result = 0;
+    for (let i = 0; i < myArray.length; i++) {
+        const element = myArray[i];
+        if (myArray[i] > result) {
+            result = myArray[i]
+        }     
+    };
+    return result;
+};
+
+console.log(`O maior valor do array é: ${maxNumber(numbers)}`);
